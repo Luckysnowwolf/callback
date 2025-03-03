02e2fd7c13e8fe4d90e572ba9446f4f05b98fab7
Luckysnowwolf <179121658+Luckysnowwolf@users.noreply.github.com>
2025-03-03 20:16:33 +0100

    added the basics

---
+++ aboutme.js
@@ -0,0 +1,23 @@
+const fetchName = () => {
+    console.log("fetching name")
+    setTimeout(() => {
+     const data = "after 2 seconds"
+    }, 2000)
+};
+
+const displayName = (data) => {
+    console.log(data)
+};
+
+fetchName(displayName);
+
+
+printName(() => { "lucas.w"
+printAge(() => { "26"
+printLocation(() => { "sweden"
+printHobbies(() => { "gaming, coding"
+
+});
+});
+});
+});
