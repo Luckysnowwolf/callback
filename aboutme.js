462429483cb2f4a12bc8bf82dfa693805b96a380
Luckysnowwolf <179121658+Luckysnowwolf@users.noreply.github.com>
2025-03-05 00:48:23 +0100

    added time and time delays

--- aboutme.js
+++ aboutme.js
@@ -1,8 +1,33 @@
 const fetchName = () => {
     console.log("fetching name")
     setTimeout(() => {
-     const data = "after 2 seconds"
-    }, 2000)
+     const data = "my name is lucas.w"
+     displayName(data)
+    },2000)
+    setTimeout(() => {
+    console.log("fetching age")
+    setTimeout(() => {
+        console.log("waiting 2 seconds")
+     const data = "my age is 26"
+     displayName(data)
+    },2000)
+},4000);
+    setTimeout(() => {
+    console.log("fetching location")
+    setTimeout(() => {
+        console.log("waiting 2 seconds")
+     const data = "my location is sweden"
+     displayName(data)
+    },2000)
+},7000);
+    setTimeout(() => {
+    console.log("fetching hobbies")
+    setTimeout(() => {
+        console.log("waiting 2 seconds")
+     const data = "my big hobbies are coding and gaming"
+     displayName(data)
+    },2000)
+},10000);
 };
 
 const displayName = (data) => {
@@ -11,13 +36,3 @@ const displayName = (data) => {
 
 fetchName(displayName);
 
-
-printName(() => { "lucas.w"
-printAge(() => { "26"
-printLocation(() => { "sweden"
-printHobbies(() => { "gaming, coding"
-
-});
-});
-});
-});
